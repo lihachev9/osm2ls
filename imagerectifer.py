5cb1c221636d20cf5b386abf5fc63ff94ab42d4c
lihachev9 <andreyyy.lihachev@gmail.com>
2025-05-31 13:57:31 +0300

    remove space in class ImageRectifier

--- imagerectifer.py
+++ imagerectifer.py
@@ -43,28 +43,28 @@ class ImageRectifier:
         rows, cols = src.shape
         # Создаем координатную сетку
         points = np.mgrid[0:np.uint16(cols), 0:np.uint16(rows)].reshape((2, rows*cols))
-        
+
         # Применяем преобразование
         transformed_points = self.a.dot(points).round().astype('int32')
         transformed_points[0] += self.offset[0]
         transformed_points[1] += self.offset[1]
-        
+
         # Преобразуем координаты и создаем новое изображение
         x_coords, y_coords = transformed_points.reshape((2, rows, cols), order='F')
         indices = x_coords + cols * y_coords
         return np.take(src, indices, mode='wrap')[:int(height) + 1, :int(width) + 1]
-    
+
     def inverse_transform(self, x: int, y: int) -> np.ndarray:
         """
         Выполняет обратное аффинное преобразование координат.
-        
+
         Args:
             x: X-координата точки
             y: Y-координата точки
-            
+
         Returns:
             Массив [x', y'] - преобразованные координаты
         """
-        transformed_point  = self.a_inv.dot([x, y])
+        transformed_point = self.a_inv.dot([x, y])
         transformed_point -= self.offset_inv
         return transformed_point.round().astype('int32')
